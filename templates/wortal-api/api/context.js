cbb3dfa0c7cb2a5ac3e9dcef008101b939d2d4d6
Tim Dillon <tim@itsdits.com>
2023-07-03 11:25:37 +0900

    feat(context): add new payload types (#30)

--- templates/wortal-api/api/context.js
+++ templates/wortal-api/api/context.js
@@ -2,7 +2,7 @@ module.exports = {
     /**
      * Gets the ID of the current context.
      * @example
-     * let id = wortal.context.getId();
+     * const id = wortal.context.getId();
      * console.log(id);
      * @returns {string | null} String ID of the current context if one exists. Null if the player is playing solo or
      * if the game is being played on a platform that does not currently support context.
@@ -14,7 +14,7 @@ module.exports = {
     /**
      * Gets the type of the current context.
      * @example
-     * let type = wortal.context.getType();
+     * const type = wortal.context.getType();
      * console.log(type);
      * @returns {contextType} The type of the current context. Possible values:
      * <ul>
@@ -56,13 +56,9 @@ module.exports = {
      * to switch into that context, and resolve if successful. Otherwise, if the player exits the menu or the client fails
      * to switch into the new context, this function will reject.
      * @example
-     * wortal.context.chooseAsync({
-     *     image: 'data:base64Image',
-     *     text: 'Invite text',
-     *     caption: 'Play',
-     *     data: { exampleData: 'yourData' },
-     * })
-     * @param {contextPayload} payload Object defining the options for the context choice.
+     * wortal.context.chooseAsync()
+     *  .then(console.log(wortal.context.getId()));
+     * @param {choosePayload} payload Object defining the options for the context choice.
      * @returns {Promise<void>} A promise that resolves with an array of player IDs of the players that were invited.
      * @throws {errorMessage} See error.message for details.
      * <ul>
@@ -175,10 +171,10 @@ module.exports = {
      * wortal.context.shareAsync({
      *     image: 'data:base64image',
      *     text: 'Share text',
-     *     caption: 'Play',
+     *     cta: 'Play',
      *     data: { exampleData: 'yourData' },
      * }).then(result => console.log(result); // Contains shareCount with number of friends the share was sent to.
-     * @param {contextPayload} payload Object defining the share message.
+     * @param {sharePayload} payload Object defining the share message.
      * @returns {Promise<number>} Number of friends the message was shared with. Facebook will return 0.
      * @throws {errorMessage} See error.message for details.
      * <ul>
@@ -209,7 +205,7 @@ module.exports = {
      *    data: { exampleData: 'yourData' },
      * })
      * .then(() => resumeGame);
-     * @param payload Object defining the payload for the custom link.
+     * @param {linkSharePayload} payload Object defining the payload for the custom link.
      * @returns {Promise<void>} Promise that resolves when the dialog is closed.
      * @throws {errorMessage} See error.message for details.
      * <ul>
@@ -230,10 +226,10 @@ module.exports = {
      * wortal.context.updateAsync({
      *     image: 'data:base64image',
      *     text: 'Update text',
-     *     caption: 'Play',
+     *     cta: 'Play',
      *     data: { exampleData: 'yourData' },
      * });
-     * @param {contextPayload} payload Object defining the update message.
+     * @param {updatePayload} payload Object defining the update message.
      * @returns {Promise<void>} Promise that resolves when the update is sent.
      * @throws {errorMessage} See error.message for details.
      * <ul>
@@ -263,38 +259,4 @@ module.exports = {
     isSizeBetween(min, max) {
         return window.Wortal.context.isSizeBetween(min, max);
     }
-
-    /**
-     * @typedef contextPayload
-     * @property {string} image URL of base64 encoded image to be displayed. This is required for the payload to be sent.
-     * @property {string | localizableContent} text Message body. This is required for the payload to be sent.
-     * @property {string | localizableContent | undefined} caption Text of the call-to-action button.
-     * @property {string | localizableContent | undefined} cta Text of the call-to-action button.
-     * @property {Record<string, unknown> | undefined} data Object passed to any session launched from this context message.
-     * Its size must be <=1000 chars when stringified.
-     * It can be accessed from `wortal.context.getEntryPointData()`
-     * @property {[contextFilter] | undefined} filters An array of filters to be applied to the friend list. Only the first filter is currently used.
-     * @property {number | undefined} maxSize Context maximum size.
-     * @property {number | undefined} minSize Context minimum size.
-     * @property {number | undefined} hoursSinceInvitation Specify how long a friend should be filtered out after the current player sends them a message.
-     * This parameter only applies when `NEW_INVITATIONS_ONLY` filter is used.
-     * When not specified, it will filter out any friend who has been sent a message.
-     * @property {string | localizableContent | undefined} description Optional customizable text field in the share UI.
-     * This can be used to describe the incentive a user can get from sharing.
-     * @property {intent | undefined} intent Message format to be used. There's no visible difference among the available options.
-     * @property {ui | undefined} ui Optional property to switch share UI mode.
-     * DEFAULT: Serial contact card with share and skip button.
-     * MULTIPLE: Selectable contact list.
-     * @property {number | undefined} minShare Defines the minimum number of players to be selected to start sharing.
-     * @property {strategy | undefined} strategy Defines how the update message should be delivered.
-     * IMMEDIATE: will be sent immediately.
-     * LAST: when the game session ends, the latest payload will be sent.
-     * IMMEDIATE_CLEAR: will be sent immediately, and also discard any pending `LAST` payloads in the same session.
-     * @property {notifications | undefined} notifications Specifies if the message should trigger push notification.
-     * @property {shareDestination | undefined} shareDestination Specifies where the share should appear.
-     * @property {boolean} switchContext Should the player switch context or not.
-     * @property {action | undefined} action Not used
-     * @property {string | undefined} template Not used
-     */
-
 }
--- templates/wortal-api/interfaces/context.js
+++ templates/wortal-api/interfaces/context.js
@@ -25,6 +25,88 @@
  */
 
 /**
+ * @typedef choosePayload
+ * Payload for context.chooseAsync. Defines the filters and search parameters to apply to the friend list.
+ * @property {string | localizableContent | undefined} description Optional customizable text field in the share UI.
+ * This can be used to describe the incentive a user can get from sharing.
+ * @property {[contextFilter] | undefined} filters An array of filters to be applied to the friend list. Only the first filter is currently used.
+ * @property {number | undefined} hoursSinceInvitation Specify how long a friend should be filtered out after the current player sends them a message.
+ * This parameter only applies when `NEW_INVITATIONS_ONLY` filter is used.
+ * When not specified, it will filter out any friend who has been sent a message.
+ *
+ * PLATFORM NOTE: Viber only.
+ * @property {number | undefined} maxSize Context maximum size.
+ * @property {number | undefined} minSize Context minimum size.
+ */
+
+/**
+ * @typedef sharePayload
+ * Payload for context.shareAsync. Defines the message to be sent to the context.
+ * @property {string | localizableContent | undefined} cta Text of the call-to-action button.
+ * @property {Record<string, unknown> | undefined} data Object passed to any session launched from this context message.
+ * Its size must be <=1000 chars when stringified.
+ * It can be accessed from `wortal.context.getEntryPointData()`
+ * @property {string | localizableContent | undefined} description Optional customizable text field in the share UI.
+ * This can be used to describe the incentive a user can get from sharing.
+ * @property {[contextFilter] | undefined} filters An array of filters to be applied to the friend list. Only the first filter is currently used.
+ * @property {number | undefined} hoursSinceInvitation Specify how long a friend should be filtered out after the current player sends them a message.
+ * This parameter only applies when `NEW_INVITATIONS_ONLY` filter is used.
+ * When not specified, it will filter out any friend who has been sent a message.
+ *
+ * PLATFORM NOTE: Viber only.
+ * @property {string} image Data URL of base64 encoded image to be displayed. This is required for the payload to be sent.
+ * @property {intent | undefined} intent Message format to be used. There's no visible difference among the available options.
+ * @property {number | undefined} minShare Defines the minimum number of players to be selected to start sharing.
+ * @property {string[] | undefined} playerIds Optional property to directly send share messages to multiple players with a confirmation prompt.
+ * Selection UI will be skipped if this property is set.
+ *
+ * PLATFORM NOTE: Viber only.
+ * @property {string | localizableContent} text A text message, or an object with the default text as the value of 'default'
+ * and another object mapping locale keys to translations as the value of 'localizations'.
+ * @property {ui | undefined} ui Optional property to switch share UI mode.
+ * @property {shareDestination[] | undefined} shareDestination An optional array to set sharing destinations in the share dialog.
+ * If not specified all available sharing destinations will be displayed.
+ *
+ * PLATFORM NOTE: Facebook only.
+ * @property {boolean} switchContext A flag indicating whether to switch the user into the new context created on sharing.
+ *
+ * PLATFORM NOTE: Facebook only.
+ */
+
+/**
+ * @typedef updatePayload
+ * Payload for context.updateAsync. Defines the message to be sent to the context.
+ * @property {action | undefined} action Message format to be used. Not currently used.
+ * @property {string | localizableContent | undefined} cta Text of the call-to-action button.
+ * @property {Record<string, unknown> | undefined} data Object passed to any session launched from this context message.
+ * Its size must be <=1000 chars when stringified.
+ * It can be accessed from `wortal.context.getEntryPointData()`
+ * @property {string} image Data URL of base64 encoded image to be displayed. This is required for the payload to be sent.
+ * @property {mediaParams | undefined} media Optional content for a gif or video. At least one image or media should be provided in order to render the update.
+ *
+ * PLATFORM NOTE: Facebook only.
+ * @property {notifications | undefined} notifications Specifies if the message should trigger push notification.
+ * @property {strategy | undefined} strategy Defines how the update message should be delivered.
+ * @property {string | undefined} template ID of the template this custom update is using. Templates should be predefined in fbapp-config.json.
+ * See the [Bundle Config documentation](https://developers.facebook.com/docs/games/instant-games/bundle-config)
+ * for documentation about fbapp-config.json.
+ *
+ * PLATFORM NOTE: Facebook only.
+ * @property {string | localizableContent} text A text message, or an object with the default text as the value of 'default'
+ * and another object mapping locale keys to translations as the value of 'localizations'.
+ */
+
+/**
+ * @typedef linkSharePayload
+ * Represents a custom link to be shared by the user.
+ * @property {string | undefined} image A base64 encoded image to be shown for the link preview. The image is recommended to either be a square or of
+ * the aspect ratio 1.91:1.
+ * @property {string | undefined} text A text description for the link preview. Recommended to be less than 44 characters.
+ * @property {Record<string, unknown> | undefined} data A blob of data to associate with the shared link. All game sessions launched from the share will be able to
+ * access this blob through Wortal.session.getEntryPointData().
+ */
+
+/**
  * @typedef localizableContent
  * Enable passing localizable content to API calls.
  * SDK will use the current player's locale for locale matching.
@@ -48,4 +130,17 @@
  * @property {number | undefined} maxResults Optional maximum number of results to include in the section. This can be no higher than 10. This will be
  * disregarded for the last section, which will contain as many results as possible. If not included, the default
  * maximum number of results for that section type will be used.
+ */
+
+/**
+ * @typedef mediaParams
+ * Represents the media payload used by custom update and custom share.
+ * @property {mediaContent | undefined} gif URL of the gif to be displayed.
+ * @property {mediaContent | undefined} video URL of the video to be displayed.
+ */
+
+/**
+ * @typedef mediaContent
+ * Specifies the content for media.
+ * @property {string} url
  */
